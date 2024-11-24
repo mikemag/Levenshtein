1ea4171eae8d4b07067c567acdac58800e16507b
TristenYim <unfathomy@proton.me>
2024-11-23 18:02:07 -0800

    Setup LevenshteinDatabase
    LevenshteinDatabase will be overridden when the wildcard identity
    algorithm is implemented. Also eventually plan to move some of the
    functionality from LevenshteinGraph into an override of
    LevenshteinDatabase, as handling both graph operations and finding
    neighbors is too large of a scope.

---
+++ src/LevenshteinDatabase.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.util.*;
+
+public abstract class LevenshteinDatabase {
+    private final String[] dictionary;
+
+    protected LevenshteinDatabase(String dictionarySourcePath) throws FileNotFoundException {
+        dictionary = makeDictionary(dictionarySourcePath);
+    }
+
+    /**
+     * Reads a file at the specified location and returns a word
+     * list generated reading the file. 
+     *
+     * @param sourcepath path to the file containing the word list.
+     *                   This file must be sorted sequentially with
+     *                   alphabetical order being the tiebreaker,
+     *                   contain one and only one word per line,
+     *                   and contain only lowercase letters
+     * @return the generated dictionary
+     */
+    public static String[] makeDictionary(String sourcepath) throws FileNotFoundException {
+        ArrayList<String> dictionaryList = new ArrayList();
+        Scanner source = new Scanner(new File(sourcepath));
+
+        while(source.hasNext()) {
+            dictionaryList.add(source.next());
+        }
+        source.close();
+
+        return dictionaryList.toArray(new String[0]);
+    }
+
+    /**
+     * Returns the dictionary of all valid words in the database. 
+     *
+     * @return the dictionary
+     */
+    public final String[] getDictionary() {
+        return dictionary;
+    };
+
+    /**
+     * Returns the set all neighbors of a word
+     *
+     * @param word the word to find the neighbors of
+     * @return the set of neighbors
+     */
+    public abstract HashSet<String> findNeighbors(String word);
+
+    /**
+     * Returns true if and only if two words are neighboring.
+     * While this shouldn't be needed for good Levenshtein
+     * algorithms, it has been left as public.
+     *
+     * @param word1 the first word
+     * @param word2 the the second word
+     * @return if the neighbors are neighboring
+     */
+    public abstract boolean areNeighbors(String word1, String word2);
+}
