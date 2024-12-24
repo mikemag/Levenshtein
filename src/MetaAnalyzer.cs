37738cc1c901920ce499cdbc0630a99c2da1af11
TristenYim <unfathomy@proton.me>
2024-12-23 18:48:30 -0800

    Enable multithreaded path diagnostics collection for entire dictionaries
    Multithreading is necessary in order for the analysis of large
    dictionaries to take an hour instead of hours. Each thread is assigned
    partitions of words which are equally spaced apart in order to ensure
    each thread is always working without complex scheduling (word position
    is correlated with the corresponding graph size). Will eventually need
    to add command-line flags to change the partitioning parameters and
    more readable output.

--- src/MetaAnalyzer.cs
+++ src/MetaAnalyzer.cs
@@ -1,9 +1,32 @@
 public class MetaAnalyzer {
     public static void Analyze(LevenshteinDatabase database) {
         const int count = 1;
-        FileStream output = new FileStream("/home/fathom/temp2.txt", FileMode.Create);
-        GraphToBinary(count, 1, 0, database, output);
+        const int partitionsPerThread = 100;
+        const int threads = 16;
+        int partitionSize = database.Words.Count() / partitionsPerThread / threads;
 
+        for (int t = 0; t < threads; t++) {
+            Thread thread = new Thread(() => ThreadGraphDiagnosis(threads, t, partitionsPerThread, partitionSize, database));
+            thread.Start();
+        }
+
+        List<PathDiagnostics> maxLengths = new List<PathDiagnostics>();
+        List<PathDiagnostics> maxPaths = new List<PathDiagnostics>();
+        maxLengths.Add(new PathDiagnostics(0, 0, 0, 0));
+        maxPaths.Add(new PathDiagnostics(0, 0, 0, 0));
+
+        for (int i = threads * partitionsPerThread * partitionSize; i < database.Words.Count(); i++) {
+            MakeGraphDiagnostics(i, database, maxLengths, maxPaths);
+        }
+        ThreadPool.SetMinThreads(0, 1);
+        ThreadPool.SetMaxThreads(threads, 1);
+
+        foreach (PathDiagnostics path in maxLengths) {
+            Console.WriteLine("(excess): " + path.ToString());
+        }
+        foreach (PathDiagnostics path in maxPaths) {
+            Console.WriteLine("(excess): " + path.ToString());
+        }
     }
 
     private static void GraphToBinary(int max, int partitions, int offset, LevenshteinDatabase database, FileStream output) {
@@ -22,8 +45,44 @@ public class MetaAnalyzer {
         output.Write(graphStream.ToArray());
         graphStream.Dispose();
         writer.Dispose();
+    }
+
+    private static void AddPathsForPartition(int max, int partitions, int offset, LevenshteinDatabase database, FileStream output) {
+        List<MemoryStream> paths = new List<MemoryStream>();
+
+        for (int i = offset; i < max; i += partitions) {
+            LevenshteinGraph graph = new LevenshteinGraph(i);
+
+            while (graph.GenerateNewOuter(database)) {
+                graph.WritePathStreams(database.Words.Count(), paths);
+            }
         }
 
+        foreach (MemoryStream stream in paths) {
+            output.Write(stream.ToArray());
+            stream.Dispose();
+        }
+    }
+
+    private static void ThreadGraphDiagnosis(int threads, int thread, int partitions, int size, LevenshteinDatabase database) {
+        List<PathDiagnostics> maxLengths = new List<PathDiagnostics>();
+        List<PathDiagnostics> maxPaths = new List<PathDiagnostics>();
+        maxLengths.Add(new PathDiagnostics(0, 0, 0, 0));
+        maxPaths.Add(new PathDiagnostics(0, 0, 0, 0));
+
+        for (int i = 0; i < partitions; i++) {
+            for (int j = thread + i * threads; j < size * partitions * threads; j += partitions * threads) {
+                /*Console.WriteLine(j);*/
+                MakeGraphDiagnostics(j, database, maxLengths, maxPaths);
+            }
+            Console.WriteLine("Done with partition (" + thread + "): " + (i + 1) + " out of " + partitions);
+        }
+
+        foreach (PathDiagnostics path in maxLengths) {
+            Console.WriteLine(path.ToString());
+        }
+        foreach (PathDiagnostics path in maxPaths) {
+            Console.WriteLine(path.ToString());
         }
     }
 
